ea60ee08a5abe68493c2d64740ebebdd01db97f3
Jarom Roney <byutrueblue@gmail.com>
2018-02-16 20:52:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,10 @@
 var playlist = {'Taylor Swift': 'Fearless'}
 
+function updatePlaylist (playlist,artist,title){
+  playlist.artist = title
+  return playlist
+}
+
+function removeFromPlaylist (playlist,artist){
+  
+}
